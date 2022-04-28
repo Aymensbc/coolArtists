34ecc05c8c59534dd92752991af79c0bf91b94dc
Aymen <aymensbc@gmail.com>
2022-04-29 01:03:48 +0500

    Create a cool search bar

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require(path);
+const path = require("path");
 const app = express();
 
 app.use("/static", express.static(path.resolve("frontend", "static")));
